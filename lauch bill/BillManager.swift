8475efb8c3671a60c81255b5fa651a47d1a88720
feeloc <hjm1989@gmail.com>
2014-06-06 17:43:45 +0800

    project init

---
+++ lauch bill/BillManager.swift
@@ -0,0 +1,13 @@
+//
+//  BillManager.swift
+//  lauch bill
+//
+//  Created by hujianmeng on 14-6-6.
+//  Copyright (c) 2014年 feeloc. All rights reserved.
+//
+
+import UIKit
+
+class BillManager: NSObject {
+   
+}
